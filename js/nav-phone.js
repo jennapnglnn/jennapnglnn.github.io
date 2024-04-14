4ce953c6cfbeb75e0618188e40ea23ce772c9209
Jenna Pangilinan <jennapangilinan19@gmail.com>
2024-04-14 12:40:09 +0800

    Update responsiveness nav

---
+++ js/nav-phone.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function() {
+    var navbarLinks = document.getElementById('navbar-links');
+    navbarLinks.classList.toggle('active');
+});
