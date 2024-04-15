f09ac0ca33ead63618e49fcc34f0d899d2a5d153
Jenna Pangilinan <jennapangilinan19@gmail.com>
2024-04-16 02:20:49 +0800

    Updated About Page

--- js/circle.js
+++ js/circle.js
@@ -1,36 +1,40 @@
-const skills = document.querySelectorAll('.skill');
-const skillsList = ['Web Design', 'Graphic Design', 'UI/UX Design', 'Drawing', 'Photoshop', 'Premier'];
+document.addEventListener('DOMContentLoaded', function() {
+    const skills = document.querySelectorAll('.skill');
+    const skillsList = ['Web Design', 'Graphic Design', 'UI/UX Design', 'Drawing', 'Photoshop', 'Premier',
+    'Web Design', 'Graphic Design', 'UI/UX Design', 'Drawing', 'Photoshop', 'Premier'];
 
-const intervalIds = [];
+    const intervalIds = [];
 
-skills.forEach((skill, index) => {
-    const number = skill.querySelector('.number');
-    const outerCircle = skill.querySelector('.outer');
-    const innerCircle = skill.querySelector('.outer .inner');
-    const percentage = parseInt(skill.dataset.num);
-    let counter = 0;
+    skills.forEach((skill, index) => {
+        const number = skill.querySelector('.number');
+        const outerCircle = skill.querySelector('.outer');
+        const innerCircle = skill.querySelector('.outer .inner');
+        const percentage = parseInt(skill.dataset.num);
+        let counter = 0;
 
-    // Skill Color
-    const color = '#EA3791';
+        // Skill Color
+        const color = '#EA3791';
 
-    const i = index; 
+        const i = index; 
 
-    const intervalId = setInterval(() => {
-        if (counter === percentage) {
-            clearInterval(intervalId);
-            number.innerHTML = skillsList[i];
-        } else {
-            counter += 1;
-            const rotation = (counter / 100) * 360; // Calculate rotation angle
-            const gradient = `conic-gradient(${color} ${rotation}deg, transparent ${rotation}deg 360deg)`;
-            outerCircle.style.background = gradient;
+        // Error handling: check if elements exist before accessing them
+        if (number && outerCircle && innerCircle) {
+            const intervalId = setInterval(() => {
+                if (counter === percentage) {
+                    clearInterval(intervalId);
+                    number.innerHTML = skillsList[i];
+                } else {
+                    counter += 1;
+                    const rotation = (counter / 100) * 360; // Calculate rotation angle
+                    const gradient = `conic-gradient(${color} ${rotation}deg, transparent ${rotation}deg 360deg)`;
+                    outerCircle.style.background = gradient;
+                }
+            }, 20);
+    
+            intervalIds.push(intervalId); 
+    
+            // Set a solid color for the inner circle
+            innerCircle.style.backgroundColor = '#FFFFFF'; 
         }
-    }, 20);
-
-    intervalIds.push(intervalId); 
-
-    // Set a solid color for the inner circle
-    innerCircle.style.backgroundColor = '#FFFFFF'; 
+    });
 });
-
-
